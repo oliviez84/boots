b2a8e8a2775139d9354a899dddded4613034a336
olivier <42643094+oliviez84@users.noreply.github.com>
2018-09-28 08:30:33 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();  
+        
+    $('.collapse').on('shown.bs.collapse', function(){
+$(this).parent().find(".fa-angle-double-down").removeClass("fa-angle-double-down").addClass("fa-angle-double-up");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".fa-angle-double-up").removeClass("fa-angle-double-up").addClass("fa-angle-double-down");
+});
+});
